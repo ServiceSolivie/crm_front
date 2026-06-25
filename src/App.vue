83b03b8aff522c16dc90b05798391da57558f5e9
Akkaoui Mohamed <akkaouimohamed00@gmail.com>
2026-06-25 11:21:10 +0200

    reload issue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script setup>
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
+import { computed, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/auth.store'
 import AuthLayout from '@/layouts/AuthLayout.vue'
 import DashboardLayout from '@/layouts/DashboardLayout.vue'
 import ToastContainer from '@/components/base/ToastContainer.vue'
 import ConfirmDialog from '@/components/base/ConfirmDialog.vue'
 
 const route = useRoute()
+const router = useRouter()
+const auth = useAuthStore()
+
+const routerReady = ref(false)
+router.isReady().then(() => { routerReady.value = true })
+
+const isBooting = computed(() => {
+  if (!auth.token) return false
+  return !routerReady.value
+})
 
 const layout = computed(() => {
   const l = route.meta?.layout
@@ -16,11 +27,15 @@ const layout = computed(() => {
 </script>
 
 <template>
-  <component :is="layout">
-    <RouterView />
-  </component>
+  <div v-if="isBooting" class="min-h-screen flex items-center justify-center bg-gray-50">
+    <div class="animate-spin rounded-full h-8 w-8 border-2 border-primary border-t-transparent" />
+  </div>
 
-  <!-- Global notification layer -->
-  <ToastContainer />
-  <ConfirmDialog />
+  <template v-else>
+    <component :is="layout">
+      <RouterView />
+    </component>
+    <ToastContainer />
+    <ConfirmDialog />
+  </template>
 </template>
